87fccf54d8a1b06b241ac85de8d34245a0b71c18
keepgoing-228 <71744036+keepgoing-228@users.noreply.github.com>
2021-03-22 22:45:39 +0800

    update sorting2
    修改compar function

--- sorting2.c
+++ sorting2.c
@@ -7,15 +7,12 @@
 #define NAME_MAX 10
 
 
-struct piggies{
-    int num;
-    char name[NAME_MAX];
-    float weight;
-}part1;
+
 
 
 void swap(int *smallest, int *first_element);
 int compareInt(const void *pa, const void *pb);
+int compareStr(const void *pa, const void *pb);
 void selection_sorts(void *base, size_t num, size_t size, int (*compare)(const void *, const void *));
 void bubble_sorts(void *base, size_t num, size_t size, int (*compare)(const void *, const void *));
 void insertion_sorts(void *base, size_t num, size_t size, int(*compare)(const void *,const void *));
@@ -24,15 +21,27 @@ void insertion_sorts(void *base, size_t num, size_t size, int(*compare)(const vo
 
 int main(){
     int array[] = {23, 78, 45, 8, 32, 56};
-    int n = sizeof(array)/sizeof(int);
-    insertion_sorts(array,n,sizeof(int),compareInt);
-    printf("this is after sorting array\n");
-    for(int i = 0; i < n; i++){
+    char name[][20] = {"Tiffany","Adam","Tim","Julia","Mike","Kelly"};
+    int n1 = sizeof(array)/sizeof(int);
+    int n2 = sizeof(name) / 20;
+    insertion_sorts(array,n1,sizeof(int),compareInt);
+    printf("----after sorting Int array\n");
+    for(int i = 0; i < n1; i++){
         printf("%d ",array[i]);
     }
-    struct piggies part1 = {"acho" , 3};
-    printf("\n%d",part1.weight);
+    printf("\n");
+
+    qsort(name,n2,20,compareStr);
+    printf("----after qsorting String array\n");
+    for(int j = 0; j < n2; j++){
+        printf("%s\n",name[j]);
+    }
     
+    selection_sorts(name,n2,20,compareStr); 
+    printf("----after selection sorting String array\n");
+    for(int j = 0; j < n2; j++){
+        printf("%s\n",name[j]);
+    }
     return 0;
 }
 
@@ -47,6 +56,9 @@ int compareInt(const void *pa, const void *pb){
     const int *p1 = pa;
     const int *p2 = pb;
     return *p1 - *p2;
+}
+int compareStr(const void *pa, const void *pb){
+    return strcmp(pa,pb);
 }
 void selection_sorts(void *base, size_t num, size_t size, int (*compare)(const void *, const void *)){
     int i,j;
